d813e81ed643c10bff67cad829a83f7054627c64
matu.dev <mati.a.gimenez2002@gmail.com>
2025-08-13 10:25:25 -0300

    clase 2

--- Clase-2-ReactJS/src/App.jsx
+++ Clase-2-ReactJS/src/App.jsx
@@ -1,45 +1,16 @@
 import React from 'react'
 import Title from './Components/Title/Title'
 import ProductList from './Components/ProductList/ProductList'
+import HomeScreen from './Screens/HomeScreen/HomeScreen'
 
 
 function App() {
 
-  const products = [
-    {
-      title: 'Tv samsung 32"',
-      id: 1,
-      price: 200,
-      descripcion: 'Tv muy buena',
-      stock: 3
-    },
-    {
-      title: 'Tv samsung 42"',
-      id: 2,
-      price: 300,
-      descripcion: 'Tv muy buena',
-      stock: 2
-    },
-    {
-      title: 'Tv samsung 52"',
-      id: 3,
-      price: 400,
-      descripcion: 'Tv muy buena',
-      stock: 1
-    }
-  ]
+
 
   return (
     <div>
-      <Title heading='h1' >
-        Hola <span style={{color: 'blue'}}>mundo</span>
-      </Title>
-
-      <Title heading='h2' >
-        Hola <span style={{color: 'blue'}}>mundo</span>
-      </Title>
-
-      <ProductList products={products}/>
+      <HomeScreen/>
     </div>
   )
 }
---
+++ Clase-2-ReactJS/src/Screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Title from '../../Components/Title/Title'
+import ProductList from '../../Components/ProductList/ProductList'
+
+
+
+function HomeScreen() {
+
+    const products = [
+        {
+            title: 'Tv samsung 32"',
+            id: 1,
+            price: 200,
+            descripcion: 'Tv muy buena',
+            stock: 3
+        },
+        {
+            title: 'Tv samsung 42"',
+            id: 2,
+            price: 300,
+            descripcion: 'Tv muy buena',
+            stock: 2
+        },
+        {
+            title: 'Tv samsung 52"',
+            id: 3,
+            price: 400,
+            descripcion: 'Tv muy buena',
+            stock: 1
+        }
+    ]
+
+    return (
+        <div>
+            <Title heading='h1' >
+                Hola <span style={{ color: 'blue' }}>mundo</span>
+            </Title>
+
+            <Title heading='h2' >
+                Hola <span style={{ color: 'blue' }}>mundo</span>
+            </Title>
+
+            <ProductList products={products} />
+        </div>
+    )
+}
+
+export default HomeScreen

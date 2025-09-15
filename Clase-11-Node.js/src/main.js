0d62c7c2eed1b93f3a2b73fb2cb40c357f429c9e
matu.dev <mati.a.gimenez2002@gmail.com>
2025-09-15 12:07:40 -0300

    clase 11

--- Clase-11-Node.js/src/main.js
+++ Clase-11-Node.js/src/main.js
@@ -1,5 +1,6 @@
 import connectToMongoDB from "./config/configMongoDB.config.js";
 import User from "./models/User.model.js";
+import {promises as filesystem} from 'fs'
 
 connectToMongoDB()
 
@@ -18,4 +19,34 @@ async function crearUsuario (name, email, password){
     }
 }
 
-crearUsuario('pepe', 'pepe@gmail.com', 'Pepe_123')
+//crearUsuario('pepe', 'pepe@gmail.com', 'Pepe_123')
+
+async function crearJSON (filename, objeto){
+    try{
+        await filesystem.writeFile(filename, JSON.stringify(objeto))
+        console.log(filename, 'creado con exito')
+    }
+    catch(error){
+
+    }
+}
+
+crearJSON('objeto_2_grande', {
+    name: 'pepe',
+    email: 'pepe@gmail.com',
+    dato: 1,
+    dato2: 2,
+    dato3: 3,
+    dato4: 4,
+    dato5: 5,
+    dato6: 6,
+    dato7: 7
+
+})
+
+crearJSON('objeto_1', {
+    name: 'pepe',
+    email: 'pepe@gmail.com',
+    password: 'Pepe_123'
+})
+

eed6946b56e414c6a20db2e36e528974dbaafafb
matu.dev <mati.a.gimenez2002@gmail.com>
2025-09-24 11:26:52 -0300

    clase 14

--- Clase-14-Node.js/src/main.js
+++ Clase-14-Node.js/src/main.js
@@ -2,6 +2,7 @@ import connectToMongoDB from "./config/configMongoDB.config.js";
 import express from 'express'
 import authRouter from "./routes/auth.router.js";
 import workspaceRouter from "./routes/workspace.router.js";
+import randomMiddleware from "./middlewares/random.middleware.js";
 
 connectToMongoDB()
 
@@ -15,10 +16,47 @@ app.use('/api/auth', authRouter)
 app.use('/api/workspace', workspaceRouter)
 
 
+
+//Quiero que falle cuando sea mayor a 0.5
+app.get(
+    '/ping',
+    /* Middleware */
+    randomMiddleware(0.5),
+    (request, response) => {
+        console.log("llego un ping")
+        response.send({ ok: true })
+    }
+)
+
+
+//Quiero que falle cuando sea mayor a 0.1
+app.get(
+    '/ping-2',
+    /* Middleware */
+    randomMiddleware(0.1),
+    (request, response) => {
+        console.log("llego un ping")
+        response.send({ ok: true })
+    }
+)
+
+//Quiero que falle cuando sea mayor a 0.9
+app.get(
+    '/ping-3',
+    /* Middleware */
+    randomMiddleware(0.9),
+    (request, response) => {
+        console.log("llego un ping")
+        response.send({ ok: true })
+    }
+)
+
+
+
 const PORT = 8080
 
 app.listen(
-    PORT, 
+    PORT,
     () => {
         console.log(`Tu servidor se esta ejecutando correctamente en el puerto ${PORT}`)
     }
--- Clase-14-Node.js/src/routes/auth.router.js
+++ Clase-14-Node.js/src/routes/auth.router.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import AuthController from "../controllers/auth.controller.js";
+import { validateRequest } from "../middlewares/validateRequest.middleware.js";
+import { registerSchema } from "../schemas/auth.schema.js";
 
 //Creamos una ruta de express
 const authRouter = Router()
 
 authRouter.get(
     '/test', 
+
     (request, response) => {
         response.send({
             ok: true
@@ -13,7 +16,11 @@ authRouter.get(
     }
 )
 
-authRouter.post('/register', AuthController.register)
+authRouter.post(
+    '/register',
+    validateRequest(registerSchema),
+    AuthController.register
+)
 /* authRouter.post('/verify-email')
 authRouter.post('/login') */
 

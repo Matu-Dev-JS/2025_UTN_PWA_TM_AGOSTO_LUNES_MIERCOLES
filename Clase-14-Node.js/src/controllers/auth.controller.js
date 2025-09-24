59081fd0c792ab0286394f2e572d270210c1cc22
matu.dev <mati.a.gimenez2002@gmail.com>
2025-09-24 12:01:07 -0300

    clase 14

--- Clase-14-Node.js/src/controllers/auth.controller.js
+++ Clase-14-Node.js/src/controllers/auth.controller.js
@@ -3,23 +3,9 @@ import { ServerError } from "../error.js";
 class AuthController {
     static async register (request, response){
         try{
-            /* 
-            Estas validaciones se suelen hacer con librerias de validacion como joi 
-            */
-          /*   const {email, name, password} = request.body
-            const email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-            if(!email || !email_regex.test(email)){
-                throw new ServerError(400, 'Email no valido')
-            } */
-
-            /* 
-            Validar el name (que sea string, mayor a 4 caracteres)
-            Validar el password (que sea string mayor a 6 caracteres)
-            */
-
-            response.send({
-                ok: true
-            })
+            const { email, password, name } = request.body
+            
+            
         }
         catch(error){
             /*
